aee732f8cc4f5773b3f57a3e2b98a40d338b0274
Mounir Nessim <mmnessim@gmail.com>
2025-02-06 16:57:36 +0000

    initial commit

---
+++ env.go
@@ -0,0 +1,78 @@
+package env
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+type Env struct {
+	Items []Item
+}
+
+type Item struct {
+	Key   string
+	Value string
+}
+
+func New(filename ...string) *Env {
+	var rows []Item
+	var f string
+
+	if len(filename) == 0 {
+		f = ".env"
+	} else {
+		f = filename[0]
+	}
+
+	file, err := os.ReadFile(f)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	fileString := string(file)
+
+	lines := strings.Split(fileString, "\n")
+
+	for _, line := range lines {
+		pair := strings.Split(line, "=")
+		if len(pair) != 2 {
+			continue
+		}
+		key := pair[0]
+		value := pair[1]
+		row := Item{Key: key, Value: strings.Trim(value, "\"")}
+		rows = append(rows, row)
+	}
+
+	e := &Env{Items: rows}
+	e.checkDuplicate()
+	return e
+}
+
+// If there are multiple keys with different values, use the last value
+func (e *Env) checkDuplicate() {
+	var keys []string
+	for _, item := range e.Items {
+		keys = append(keys, item.Key)
+	}
+	for i, key := range keys {
+		for j := range len(keys) {
+			if key == keys[j] {
+				//fmt.Println("dup")
+				e.Items[i].Value = e.Items[j].Value
+			}
+		}
+	}
+}
+
+func (e *Env) Get(key string) string {
+	for _, j := range e.Items {
+		if j.Key == key {
+			return j.Value
+		} else {
+			continue
+		}
+	}
+	return ""
+}
---
+++ env_test.go
@@ -0,0 +1,35 @@
+package env
+
+import (
+	"testing"
+)
+
+func TestNoPath(t *testing.T) {
+	e := New()
+	if e.Get("TEST") != "PASS" {
+		t.Fatalf("expected PASS got %s", e.Get("TEST"))
+	}
+}
+
+func TestPath(t *testing.T) {
+	e := New(".env.local")
+	if e.Get("TEST") != "PASS" {
+		t.Fatalf("expected PASS got %s", e.Get("TEST"))
+	}
+	if e.Get("BLORG") != "BLARK" {
+		t.Fatalf("expected PASS got %s", e.Get("BLORG"))
+	}
+	if e.Get("HELLO") != "WORLD" {
+		t.Fatalf("expected PASS got %s", e.Get("HELLO"))
+	}
+}
+
+func TestDuplicateKeys(t *testing.T) {
+	e := New(".env.dup")
+	if e.Get("TEST") != "PASS" {
+		t.Fatalf("expected PASS got %s", e.Get("TEST"))
+	}
+	if e.Get("TEST2") != "PASS" {
+		t.Fatalf("expected PASS got %s", e.Get("TEST2"))
+	}
+}
